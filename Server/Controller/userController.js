622082de196f476c3be6e3d2f72b329f03a8235a
sneha <sneha.2327cs1220@kiet.edu>
2026-01-20 01:04:14 +0530

    Implement user authentication with registration and login APIs

---
+++ Server/Controller/userController.js
@@ -0,0 +1,92 @@
+import User from "../Model/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const genrateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+// ============================================
+// 🔹 API: Register New User
+// ============================================
+// Route: POST /api/user/register
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User Already Exist with this email" });
+    }
+
+    //hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    //new user
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    const token = genrateToken(newUser._id);
+
+    newUser.password = undefined;
+
+    return res.status(201).json({
+      message: "User created successfully",
+      token,
+      user: newUser,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// ============================================
+// 🔹 API: Login User
+// ============================================
+// Route: POST /api/user/login
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "No User  Exist with this email" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const token = genrateToken(user._id);
+
+    user.password = undefined;
+
+    return res.status(200).json({
+      message: "User login successfully",
+      token,
+      user
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './Config/db.js';
 import 'dotenv/config';
+import userRouter from './Routes/userRoutes.js';
 
 
 const app=express();
@@ -14,6 +15,8 @@ await connectDB()
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
+
+app.use('/api/user',userRouter)
 
 const PORT = process.env.PORT || 5000;
 
